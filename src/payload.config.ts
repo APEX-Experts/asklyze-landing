14c2928d937a6b280de846acc7ebb3238240bf1a
AmrMohamed27 <amr.mohamed@myquery.ai>
2026-01-21 15:31:46 +0100

    fix: add ts extension to collection import in paylaod config

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -3,8 +3,8 @@ import { sqliteAdapter } from "@payloadcms/db-sqlite";
 import { lexicalEditor } from "@payloadcms/richtext-lexical";
 import path from "path";
 import { fileURLToPath } from "url";
-import { Users } from "./collections/Users";
-import { Posts } from "./collections/Posts";
+import { Users } from "./collections/Users.ts";
+import { Posts } from "./collections/Posts.ts";
 import sharp from "sharp";
 
 const filename = fileURLToPath(import.meta.url);
